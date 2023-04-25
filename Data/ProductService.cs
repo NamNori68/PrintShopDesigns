822357a58190d7042a4038d3deb6b9b01b3efb34
NamNori68 <marc@machin.com>
2023-04-25 11:54:13 -0700

    #3 - Products - Delete Fails - Issue is fixed

--- Data/ProductService.cs
+++ Data/ProductService.cs
@@ -9,15 +9,27 @@ using PrintShopDesigns.Pages;
 
 namespace PrintShopDesigns.Data
 {
+    /// <summary>
+    /// Represents the database services for managing the Product
+    /// </summary>
     public class ProductService : IProductService
     {
         private readonly IDapperService _dapperService;
 
+        /// <summary>
+        /// Initializes a new ProductService. 
+        /// </summary>
+        /// <param name="dapperService">The delegate that represents the Dapper Service database connection mechanism.</param>
         public ProductService(IDapperService dapperService)
         {
             this._dapperService = dapperService;
         }
 
+        /// <summary>
+        /// Creates a new Product that is added to the database.
+        /// </summary>
+        /// <param name="_product">Represents the object model of the Product to be added.</param>
+        /// <returns>The ID value of the newly created Product.</returns>
         public Task<int> Create(Product _product)
         {
             var dbPara = new DynamicParameters();
@@ -34,28 +46,42 @@ namespace PrintShopDesigns.Data
             return _productID;
         }
 
+        /// <summary>
+        /// Gets a specific Product from the database.
+        /// </summary>
+        /// <param name="id">The ID value of the Product that is to be retrieved from the database.</param>
+        /// <returns>A object of the specific Product.</returns>
         public Task<Product> GetById(int id)
         {
             var dbPara = new DynamicParameters();
 
-            dbPara.Add("CustomerID", id, DbType.Int16);
+            dbPara.Add("ProductID", id, DbType.Int16);
 
             var _product = Task.FromResult(_dapperService.Get<Product>("dbo.usp_Product_GetByID", dbPara, commandType: CommandType.StoredProcedure));
 
             return _product;
         }
 
+        /// <summary>
+        /// Deletes the current Product from the database.
+        /// </summary>
+        /// <param name="id">The ID value of the Product to be deleted.</param>
+        /// <returns>The successfully completed delete task.</returns>
         public Task<int> Delete(int id)
         {
             var dbPara = new DynamicParameters();
 
-            dbPara.Add("CustomerID", id, DbType.Int16);
+            dbPara.Add("ProductID", id, DbType.Int16);
 
             var deleteProduct = Task.FromResult(_dapperService.Execute("dbo.usp_Product_DeleteByID", dbPara, commandType: CommandType.StoredProcedure));
 
             return deleteProduct;
         }
 
+        /// <summary>
+        /// Returns a list of all Products in the database.
+        /// </summary>
+        /// <returns>The list of all Products.</returns>
         public Task<List<Product>> ListAll()
         {
             var _product = Task.FromResult(_dapperService.GetAll<Product>("dbo.usp_Product_GetAll", null, commandType: CommandType.StoredProcedure));
@@ -63,6 +89,11 @@ namespace PrintShopDesigns.Data
             return _product;
         }
 
+        /// <summary>
+        /// Updates the current Product in the database.  
+        /// </summary>
+        /// <param name="_product">Represents the object model of the Product to be updated.</param>
+        /// <returns>The successfully completed update task.</returns>
         public Task<int> Update(Product _product)
         {
             var dbPara = new DynamicParameters();
